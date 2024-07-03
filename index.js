59639dc67b60b10f67cfd405efc2b1a0c9ba4280
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-07-03 07:16:36 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+}
+
+// Define wrapAdjective function
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+}
